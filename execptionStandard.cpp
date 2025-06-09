361b00d6daa44c78c3d505ed5031021ccd836dad
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-09 19:04:26 +0700

    membuat penanda ke 2

--- execptionStandard.cpp
+++ execptionStandard.cpp
@@ -6,17 +6,21 @@
 using namespace std;
 
 int main(){
-    cout << "Awal Program" << endl // penanda 1:....
+    cout << "Awal Program" << endl; // penanda 1:....
     try
     {
-        array<int, 3> data = {10,20,30}
+        array<int, 3> data = {10,20,30};
         //pesan Array integer 3 elemen
         cout << data.at(4) << endl;
         //memanggil array elemen ke 5
     }
-    catch(const std::exception& e)
+    catch(exception &e)
     {
-        std::cerr << e.what() << '\n';
+      //penangkap menggunakan obyek exception
+      cout << e.what() << endl;
+      /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
     }
-    
+    cout << "Baris program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
