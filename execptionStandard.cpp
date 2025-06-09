36a5d809ee03d88cbf492c6a7f7be2f04fb77c8f
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-09 18:58:14 +0700

    memanggil array untuk elemen ke 5

--- execptionStandard.cpp
+++ execptionStandard.cpp
@@ -11,6 +11,8 @@ int main(){
     {
         array<int, 3> data = {10,20,30}
         //pesan Array integer 3 elemen
+        cout << data.at(4) << endl;
+        //memanggil array elemen ke 5
     }
     catch(const std::exception& e)
     {
