bf805498a4a9ea76e0f408d90555d34b8a3dc471
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-09 18:21:05 +0700

    membuka file dalam mode menulis di fungsi utama

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+}
