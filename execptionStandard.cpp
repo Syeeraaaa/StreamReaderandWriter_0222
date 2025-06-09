9387c7ac40991293b69c77c446dac1acdd32506c
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-09 18:57:15 +0700

    membuat pesan array untuk integer 3 elemen

--- execptionStandard.cpp
+++ execptionStandard.cpp
@@ -7,4 +7,14 @@ using namespace std;
 
 int main(){
     cout << "Awal Program" << endl // penanda 1:....
+    try
+    {
+        array<int, 3> data = {10,20,30}
+        //pesan Array integer 3 elemen
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 }
