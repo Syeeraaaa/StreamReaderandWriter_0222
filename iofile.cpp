3223f3f4ad6f1bd1063a3a1795c04150e7573788
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-09 17:23:04 +0700

    membuat objek output file stream dalam mode menulis

---
+++ iofile.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    // membuat obyek output file stream dalam mode menulis
+    ofstream outfile;
+}
